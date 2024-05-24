44176f56e43503d87ec0270d5d94ed7335b28ac3
sebaszap123-dev <sebaszap123@gmail.com>
2024-05-23 20:53:04 -0600

    handle password and validate for users

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,31 @@
 from rest_framework import serializers
 
 from users.models import NiceUser
+from django.contrib.auth.password_validation import validate_password
 
 class NiceUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = NiceUser
-        fields = ('id', 'email',)
+        fields = ('id', 'email', 'password',)
+
+    def validate_password(self, value):
+        # Valida la contraseña usando los validadores de Django
+        validate_password(value)
+        return value
+
+    def create(self, validated_data):
+        user = NiceUser(
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        if 'password' in validated_data:
+            instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
